175fa36e46a34b3936f7d28eec0637e3306656af
ydovbak <yulia.dovbak@gmail.com>
2020-10-22 22:58:13 +0100

    Finished caret get and set functions

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 "use strict";
 
-function $(id)
-{
-	return document.getElementById(id);
+function $(id) {
+    return document.getElementById(id);
 }
 
 
-window.onload = function(){
+window.onload = function () {
 
     //Focus the input
     $("userInput").focus();
@@ -37,75 +36,127 @@ window.onload = function(){
     //Delete buttons
     $("eraseBtn").onclick = eraseInput;
     $("deleteBtn").onclick = deleteLastIput;
+
+    //$("percentBtn").onclick = getCaret;
+
+
 };
 
+function evaluate() {
+    //Get the expression from the input field
+    var inputField = $("userInput").value;
+
+    //Write the result to the output field
+    $("resultOutput").innerText = eval(inputField);
+
+    //After button input, put focus on input line
+    $("userInput").focus();
+}
 
 
 //All button clicks where button content fully corresponds to operation input needed
-function simpleBtnClicked(){
+function simpleBtnClicked() {
     //Get access to input field
-    var inputField =  $("userInput");
+    var inputField = $("userInput");
 
     //Get the number corresponding to the id
     var number = this.firstElementChild.innerHTML;
 
     //Writing the number into the input field
-    inputField.value += number;
-
-    //After button input, put focus on input line
-    $("userInput").focus();
+    putSymbbolOnPosition(number, getCaretPosition());
 };
 
 //Function that handles Multiply button click
-function multiplyBtnClicked(){
+function multiplyBtnClicked() {
     //Get access to input field 
-    var inputField =  $("userInput");
+    var inputField = $("userInput");
 
     //Writing multiply sign into the input field
-    inputField.value += "*";
-
-    //After button input, put focus on input line
-    $("userInput").focus();
+    putSymbbolOnPosition("*", getCaretPosition());
 };
 
 //Function that handles Multiply button click
-function divideBtnClicked(){
+function divideBtnClicked() {
     //Get access to input field 
-    var inputField =  $("userInput");
+    var inputField = $("userInput");
 
-    //Writing multiply sign into the input field
-    inputField.value += "/";
-
-    //After button input, put focus on input line
-    $("userInput").focus();
+    //Writing divide sign into the input field
+    putSymbbolOnPosition("/", getCaretPosition());
 };
+
+//This method is writing the synbol into the required position in input field
+function putSymbbolOnPosition(symbol, position) {
+    var input = $("userInput").value;
+    var result = input.slice(0, position) + symbol + input.slice(position);
+    $("userInput").value = result;
+
+    //Moving caret forward by 1
+    setCaretPosition(position + 1);
+}
 
 //Clear the input
-function eraseInput(){  
+function eraseInput() {
     $("userInput").value = "";
 };
 
 
 //Delete last value or sign entered into the input field
-function deleteLastIput(){
+function deleteLastIput() {
+
     //Get access to input field 
-    var inputField =  $("userInput").value;
+    var userInput = $("userInput").value;
+    var caretPosition = getCaretPosition();
 
     //Check if input field isn't empty
-    if(inputField.length > 0)
-    {
-        //This expression is getting a substring of the input that already exists without the last input
-        var res = inputField.substring(0, inputField.length - 1);
+    if (userInput.length > 0 && caretPosition > 0) {
 
-        //Saving result in input 
-        $("userInput").value = res;
+        //If caret is in the end, delete last element
+        if (caretPosition == userInput.length) {
 
-        //If the resulting input if not empty, leaving focus on input
-        if(res.length > 0)
-        {
-            $("userInput").focus();
+            $("userInput").value = userInput.substring(0, userInput.length - 1);
+
+            //If the resulting input if not empty, leaving focus in the end of the input
+            if (userInput.length > 0) {
+                $("userInput").focus();
+            }
+        } else {
+            //Deleting the element behind the caret
+            $("userInput").value = userInput.slice(0, caretPosition - 1) + userInput.slice(caretPosition, userInput.length);
+
+            //Leaving caret in the same position
+            setCaretPosition(caretPosition - 1);
         }
-        
+
     }
-    
+
+};
+
+//This method returns the position of the cursor at the moment
+function getCaretPosition() {
+    var myElement = $('userInput');
+    var startPosition = myElement.selectionStart;
+    return startPosition;
+}
+
+
+//This method is setting the cursor to required position in the input 
+function setCaretPosition(caretPos) {
+    var element = $("userInput");
+
+    //If element exists, puting caret to position
+    if (element != null) {
+        if (element.createTextRange) {
+            var range = element.createTextRange();
+            range.move('character', caretPos);
+            range.select();
+        }
+        else {
+            if (element.selectionStart) {
+                element.focus();
+                element.setSelectionRange(caretPos, caretPos);
+            }
+            else
+                element.focus();
+        }
+    }
 }
